67a43be507f91d4b2361f933dca1e6ee59f05ae3
M-B15 <mcactusdesert@icloud.com>
2025-02-13 12:53:07 -0500

    First Commit!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let message = std::env::args().nth(1)
+        .expect("Missing the message. Usage: catsy <message>");
+    println!("{}", message);
+    println!(" \\");
+    println!("  \\");
+    println!("     /\\_/\\");
+    println!("    ( o.o )");
+    println!("    =(_I_)= ");
 }
