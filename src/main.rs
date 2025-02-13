7cc1fc7638c63d9c33c96ff841509155e87e35d2
M-B15 <mcactusdesert@icloud.com>
2025-02-13 13:46:41 -0500

    Second Commit!

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,57 @@
-fn main() {
-    let message = std::env::args().nth(1)
-        .expect("Missing the message. Usage: catsy <message>");
-    println!("{}", message);
-    println!(" \\");
-    println!("  \\");
-    println!("     /\\_/\\");
-    println!("    ( o.o )");
-    println!("    =(_I_)= ");
+use colored::Colorize;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Options {
+
+    #[clap(default_value = "Meow!")]
+    /// What does the cat say?
+    message: String, // [1]
+
+    #[clap(short = 's', long = "sleeping")]
+    /// Make the cat sleep
+    sleeping: bool, // [2]
+
+    #[clap(short = 'f', long = "file")]
+    /// Load the cat picture from the specified file
+    catfile: Option<std::path::PathBuf>,
 }
+
+fn main() {
+    let options = Options::parse(); //[2]
+    let message = options.message;
+    
+    let eye = if options.sleeping { "-" } else { "o" }; //[1]
+
+    if message.to_lowercase() == "woof" {
+        eprintln!("A cat shouldn't bark like a dog."); 
+    }
+    
+    match &options.catfile {
+        Some(path) => {
+            let cat_template = std::fs::read_to_string(path)
+            .expect(
+            &format!("could not read file {:?}", path)
+            );
+            let eye = format!("{}", eye.red().bold());
+            let cat_picture = cat_template.replace("{eye}", &eye);
+            println!(
+                "{}",
+                message.bright_yellow().underline().on_purple()
+            );
+            println!("{}", cat_picture);
+        },
+        None => {
+            println!(
+                "{}",
+                message.bright_yellow().underline().on_purple()
+            );
+            println!(" \\");
+            println!("  \\");
+            println!("     /\\_/\\");
+            println!("    ( {eye}.{eye} )", eye=eye.red().bold()); // [2]
+            println!("    =(_I_)= ");
+        }
+    }
+}
+
